843e534bebbb71ee1cecf4006af7ab8d75ab49c0
Aclecio Cruz <aclecioscruz@gmail.com>
2024-08-05 16:03:43 -0300

    remover comentarios

--- src/app.js
+++ src/app.js
@@ -1,9 +1,6 @@
 import express, { json } from 'express';
 import connectDB from './config/dbConnect.js';
 import routes from './router/index.js';
-// import { collectDefaultMetics as collectDefaultMetrics, register } from 'prom-client'
-
-// collectDefaultMetrics();
 
 const connect = await connectDB();
 
@@ -18,14 +15,6 @@ connect.once("open", () => {
 const app = express();
 routes(app);
 
-// app.get("/metrics", async (req, res) => {
-//     try {
-//         res.set("content-Type", register.contentType);
-//         res.end(await register.metrics());
-//     } catch (error) {
-//         res.status(500).end(error)
-//     }
-// });
 
 
 export default app;
